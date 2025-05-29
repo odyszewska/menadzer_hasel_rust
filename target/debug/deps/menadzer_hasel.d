3c0001e6dc8c20587669d5df2971c576f1f50441
Agnieszka <agadabek.sa@gmail.com>
2025-05-29 16:12:57 +0200

    features derive

--- target/debug/deps/menadzer_hasel.d
+++ target/debug/deps/menadzer_hasel.d
@@ -1,5 +1,5 @@
-C:\Users\oliwi\Desktop\menadzer_hasel_rust\menadzer_hasel\target\debug\deps\menadzer_hasel.exe: src\main.rs
+C:\Users\agada\RustroverProjects\menadzer_hasel\target\debug\deps\menadzer_hasel.exe: src\main.rs
 
-C:\Users\oliwi\Desktop\menadzer_hasel_rust\menadzer_hasel\target\debug\deps\menadzer_hasel.d: src\main.rs
+C:\Users\agada\RustroverProjects\menadzer_hasel\target\debug\deps\menadzer_hasel.d: src\main.rs
 
 src\main.rs:
